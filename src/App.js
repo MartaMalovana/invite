eedfaf62cadb231274536ce49ef9a0d42b68fe92
MartaMalovana <marta.malovana@gmail.com>
2026-03-24 15:50:19 +0200

    red version

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import orn from "./images/white-flow.png";
-import flLeft from "./images/flower_center.png";
-import flCenter from "./images/flower_left.png";
+import orn from "./images/red-flowers.png";
+import flLeft from "./images/top_right_flowers.png";
+import flCenter from "./images/top_right_flowers.png";
+import heart from "./images/heart.svg";
 
 function App() {
-  // const [modalIsOpen, setModalIsOpen] = useState(true);
-  // const [imgStyle, setImgStyle] = useState("img-visible");
-  // const [pictureStyle, setPictureStyle] = useState("picture-hidden");
-  // const [textStyle, setTextStyle] = useState("");
-  // const [butterflyMove, setButterflyMove] = useState("");
   const [imgScaled, setImgScaled] = useState("");
+  const [textVisible, setTextVisible] = useState("");
+  const [flowerVisible, setFlowerVisible] = useState("");
+  const [rombVisible, setRombVisible] = useState("");
+  const [timingVisible, setTimingVisible] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
       setImgScaled("scaled");
     }, 0);
+    setTimeout(() => {
+      setTextVisible("visible");
+    }, 3000);
+    setTimeout(() => {
+      setFlowerVisible("visible");
+    }, 5000);
+    setTimeout(() => {
+      setRombVisible("visible");
+    }, 6000);
+    setTimeout(() => {
+      setTimingVisible("visible");
+    }, 7000);
   }, []);
-
-  // const handleHideModal = () => {
-  //   setImgStyle("img-modified");
-  //   setTimeout(() => setModalIsOpen(false), 2000);
-  // };
 
   return (
     <div className="App">
       {/* Main picture */}
       <div className="main-picture">
-        <img src={orn} alt="logo" className={`logo picture-visible first ${imgScaled}`} />
-        <div className={`text visible`}>
+        <img
+          src={orn}
+          alt="logo"
+          className={`logo picture-visible first ${imgScaled}`}
+        />
+        <div className={`text ${textVisible}`}>
           <div className="guest">
             Любі <div className="guest-name">Дмитро, Тетяна і Сашко!</div>
           </div>
@@ -38,16 +49,15 @@ function App() {
             <div className="and"></div>
             <div className="marta"></div>
           </div>
-          запрошуємо вас на наше весілля!
+          запрошуємо вас на наше весілля
           <div>30 травня 2026</div>
+          <div><img src={heart} alt="heart" style={{width: "20px"}}/></div>
         </div>
-
-        <div></div>
       </div>
       {/* Timing */}
-      <div className="timing">
-        <div className="timing-title">Таймінг весілля</div>
-        <div className="timing-item">
+      <div className={`timing`}>
+        <div className={`timing-title ${timingVisible}`}>Таймінг весілля</div>
+        <div className={`timing-item ${timingVisible}`}>
           <div className="time">11:00</div>
           <div>Вінчання</div>
           <div>Церква Св. Петра і Павла, </div>
@@ -62,7 +72,7 @@ function App() {
             </a>
           </button>
         </div>
-        <div className="timing-item">
+        <div className={`timing-item ${timingVisible}`}>
           <div className="time">15:00</div>
           <div>Церемонія одруження. Бенкет</div>
           <div>Meat Club,</div>
@@ -77,8 +87,25 @@ function App() {
             </a>
           </button>
         </div>
-        <img src={flCenter} alt="logo" className={`logo picture-visible two ${imgScaled}`} />
-        <img src={flLeft} alt="logo" className={`logo picture-visible three`} />
+        <div className={`wed-style ${timingVisible}`}>
+          <div className={`timing-title ${timingVisible}`}>Стиль весілля</div>
+          <div>
+            Етнічний стиль нашого весілля — це наша шана традиціям, любов до
+            рідної культури, та віра у світле майбутнє
+          </div>
+        </div>
+        <img
+          src={flCenter}
+          alt="flower"
+          className={`flower picture-visible two ${flowerVisible}`}
+        />
+        <img
+          src={flLeft}
+          alt="flower"
+          className={`flower picture-visible three`}
+        />
+        <div className={`rombs ${rombVisible}`}></div>
+        <div className="rombs-two"></div>
       </div>
     </div>
   );
