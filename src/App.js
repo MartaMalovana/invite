4a5ba6cc16008301d1719ca0e0cb2da27b4c2afc
MartaMalovana <marta.malovana@gmail.com>
2026-04-21 22:39:22 +0300

    adds moving flowers

--- src/App.js
+++ src/App.js
@@ -1,66 +1,129 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import orn from "./images/red-flowers.png";
 import flLeft from "./images/top_right_flowers.png";
-import flCenter from "./images/top_right_flowers.png";
 import heart from "./images/heart.svg";
+import topleft from "./images/topleft2.png";
+import topcenter from "./images/topcenter2.png";
+import topright from "./images/topright2.png";
+import bottomleft from "./images/bottomleft.png";
+import bottomcenter from "./images/bottomcenter.png";
+import bottomright from "./images/bottomright.png";
+import centerlf from "./images/center-fl.png";
+import guestList from "./guests.json";
 
 function App() {
-  const [imgScaled, setImgScaled] = useState("");
   const [textVisible, setTextVisible] = useState("");
   const [flowerVisible, setFlowerVisible] = useState("");
-  const [rombVisible, setRombVisible] = useState("");
   const [timingVisible, setTimingVisible] = useState("");
+  const [topleftFl, setTopleftFl] = useState("");
+  const [topCenterFl, setTopCenterFl] = useState("");
+  const [topRightFl, setTopRightFl] = useState("");
+  const [bottomLeftFl, setBottomLeftFl] = useState("");
+  const [bottomCenterFl, setBottomCenterFl] = useState("");
+  const [bottomRightFl, setBottomRightFl] = useState("");
+  const [greetingWord, setGreetingWord] = useState("Любі");
+  const [guestName, setGuestName] = useState("друзі");
+  const [you, setYou] = useState("вас");
 
   useEffect(() => {
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("i");
+    const guestData = guestList.find((g) => String(g.id) === String(id));
+    if (guestData) {
+      setGreetingWord(guestData.greeting);
+      setGuestName(guestData.name);
+      setYou(guestData.you);
+    }
+
     setTimeout(() => {
-      setImgScaled("scaled");
+      setTopleftFl("scaled");
     }, 0);
     setTimeout(() => {
+      setTopCenterFl("scaled");
+    }, 1500);
+    setTimeout(() => {
+      setTopRightFl("scaled");
+    }, 2500);
+    setTimeout(() => {
+      setBottomRightFl("scaled");
+    }, 3500);
+    setTimeout(() => {
+      setBottomCenterFl("scaled");
+    }, 4500);
+    setTimeout(() => {
+      setBottomLeftFl("scaled");
+    }, 5500);
+    setTimeout(() => {
       setTextVisible("visible");
-    }, 3000);
+    }, 6500);
     setTimeout(() => {
       setFlowerVisible("visible");
-    }, 5000);
-    setTimeout(() => {
-      setRombVisible("visible");
-    }, 6000);
+    }, 7000);
     setTimeout(() => {
       setTimingVisible("visible");
-    }, 7000);
+    }, 7500);
   }, []);
 
   return (
     <div className="App">
       {/* Main picture */}
       <div className="main-picture">
+        <img src={centerlf} alt="logo" className={`logo `} />
+
         <img
-          src={orn}
-          alt="logo"
-          className={`logo picture-visible first ${imgScaled}`}
+          src={topleft}
+          className={`fl-main fl-top-left ${topleftFl}`}
+          alt="flower"
+        />
+        <img
+          src={topcenter}
+          className={`fl-main fl-top-center ${topCenterFl}`}
+          alt="flower"
+        />
+        <img
+          src={topright}
+          className={`fl-main fl-top-right ${topRightFl}`}
+          alt="flower"
+        />
+        <img
+          src={bottomleft}
+          className={`fl-main fl-b-left ${bottomLeftFl}`}
+          alt="flower"
+        />
+        <img
+          src={bottomcenter}
+          className={`fl-main fl-b-center ${bottomCenterFl}`}
+          alt="flower"
+        />
+        <img
+          src={bottomright}
+          className={`fl-main fl-b-right ${bottomRightFl}`}
+          alt="flower"
         />
         <div className={`text ${textVisible}`}>
           <div className="guest">
-            Любі <div className="guest-name">мама і тато!</div>
+            {greetingWord} <div className="guest-name">{guestName}!</div>
           </div>
           Від щирого серця ми,
-          <div className="bride">
-            <div className="taras">Тарас та Марта,</div>
-            <div className="and"></div>
-            <div className="marta"></div>
-          </div>
-          запрошуємо вас на наше весілля
+          <div className="bride">Тарас та Марта,</div>
+          запрошуємо {you} на наше весілля
           <div>30 травня 2026</div>
           <div>
             <img src={heart} alt="heart" style={{ width: "20px" }} />
           </div>
         </div>
       </div>
+      {/* Rombs */}
+      <div className="flex-c">
+        <div className="rombs-middle rombs-2"></div>
+        <div className="rombs-middle"></div>
+        <div className="rombs-middle rombs-2"></div>
+      </div>
       {/* Timing */}
       <div className={`timing`}>
         <div className={`timing-title ${timingVisible}`}>Таймінг весілля</div>
         <div className={`timing-item ${timingVisible}`}>
-          <div className="time">11:00</div>
+          <div className="time">14:00</div>
           <div>Вінчання</div>
           <div>Церква Св. Петра і Павла, </div>
           <div>вул.Львівська, Зимна Вода</div>
@@ -92,12 +155,12 @@ function App() {
         <div className={`wed-style ${timingVisible}`}>
           <div className={`timing-title ${timingVisible}`}>Стиль весілля</div>
           <div>
-            Етнічний стиль нашого весілля — це наша шана традиціям, любов до
-            рідної культури, та віра у світле майбутнє
+            Етнічний стиль нашого весілля — це наша любов до рідної культури та
+            шана традиціям
           </div>
         </div>
         <img
-          src={flCenter}
+          src={topright}
           alt="flower"
           className={`flower picture-visible two ${flowerVisible}`}
         />
@@ -106,8 +169,6 @@ function App() {
           alt="flower"
           className={`flower picture-visible three`}
         />
-        <div className={`rombs ${rombVisible}`}></div>
-        <div className="rombs-two"></div>
       </div>
     </div>
   );
